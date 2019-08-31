0af31db1a0651e1aec76a928773c7051996cc5ea
Jesus <jesush.prz@gmail.com>
2019-08-30 22:54:03 -0500

    Added main article list section

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, json
+import config
+import requests
+
 app = Flask(__name__, static_folder="static")
 
 @app.route('/')
 def hello_world():
-  return render_template('index.html')
+  #url = config.news_url + config.news_key
+  #news = requests.get(url).json()
+
+  with open('test.json', encoding='utf8') as f:
+    data = json.load(f)
+  articles = data['articles']
+
+  return render_template('index.html', articles=articles)
+
+if __name__ == '__main__':
+  app.run(debug=True)
